ccbcd405453e0c7ee1491ce4ab47a7a681178f55
Md Sahil <md61421@gmail.com>
2024-07-10 14:43:35 +0530

    feat: Enable module to create and store user credentials in 'supabase_users_field_table'

--- src/SupabaseService.php
+++ src/SupabaseService.php
@@ -3,14 +3,12 @@
 namespace Drupal\supabase_authentication;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Database\Connection;
 use PHPSupabase\Service;
+use Psr\Log\LoggerInterface;
 
 /**
- * Contains \Drupal\supabase_authentication\SupabaseService.
- */
-
-/**
- * Service class for integrating with Supabase.
+ * Provides methods to interact with the Supabase authentication service.
  */
 class SupabaseService {
 
@@ -29,13 +27,33 @@ class SupabaseService {
   protected $supabaseService;
 
   /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * The logger channel.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
    * SupabaseService constructor.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database connection.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   The logger channel.
    */
-  public function __construct(ConfigFactoryInterface $config_factory) {
+  public function __construct(ConfigFactoryInterface $config_factory, Connection $database, LoggerInterface $logger) {
     $this->configFactory = $config_factory;
+    $this->database = $database;
+    $this->logger = $logger;
     $config = $this->configFactory->get('supabase_authentication.config_form');
 
     $apiKey = $config->get('api_key');
@@ -86,6 +104,48 @@ class SupabaseService {
     }
     catch (\Exception $e) {
       return $auth->getError();
+    }
+  }
+
+  /**
+   * Stores user credentials in the custom table.
+   *
+   * @param string $email
+   *   The email of the user.
+   * @param string $password
+   *   The password of the user.
+   */
+  public function storeUserCredentials($email, $password) {
+    $this->database->insert('supabase_users_field_table')
+      ->fields([
+        'email' => $email,
+        'password' => $password,
+        'created' => \Drupal::time()->getRequestTime(),
+      ])
+      ->execute();
+  }
+
+  /**
+   * Logs a message.
+   *
+   * @param string $level
+   *   The log level (e.g., 'notice', 'error').
+   * @param string $message
+   *   The log message.
+   * @param array $context
+   *   Additional context for the log message.
+   */
+  public function log($level, $message, array $context = []) {
+    $this->logger->$level($message, $context);
+  }
+
+  /**
+   * Drops the custom table.
+   */
+  public function dropTable() {
+    $schema = $this->database->schema();
+    if ($schema->tableExists('supabase_users_field_table')) {
+      $schema->dropTable('supabase_users_field_table');
     }
   }
 
